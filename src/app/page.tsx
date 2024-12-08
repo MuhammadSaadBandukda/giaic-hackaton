15e1e3126a00cb2d21200d48c70a1cdb80234e5c
Muhammad Saad <155905336+MuhammadSaadBandukda@users.noreply.github.com>
2024-12-08 22:44:00 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import Header from "@/components/Header";
 import HeroSection from "@/components/HeroSection";
 import NavBar from "@/components/NavBar";
 import TopHeader from "@/components/TopHeader";
-import Image from "next/image";
 
 export default function Home() {
   return (
