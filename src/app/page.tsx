e1db73aaff5c035159e7e3371ba8872452d0e8a3
Muhammad Saad <155905336+MuhammadSaadBandukda@users.noreply.github.com>
2024-12-08 22:48:24 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import HeroSection from "@/components/HeroSection";
-import NavBar from "@/components/NavBar";
 import TopHeader from "@/components/TopHeader";
 
 export default function Home() {
