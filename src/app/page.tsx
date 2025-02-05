966abd7333b22127071f77a0d0c361daa1fd2e9c
Muhammad Saad <155905336+MuhammadSaadBandukda@users.noreply.github.com>
2025-02-06 01:26:00 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import Brands from "@/app/components/Brands";
 import DiscountItem from "@/app/components/DiscountItem";
 import FeaturedProd from "@/app/components/FeaturedProd";
@@ -11,9 +11,8 @@ import ShopexOffer from "@/app/components/ShopexOffer";
 import TopCategory from "@/app/components/TopCategory";
 import TrendingProducts from "@/app/components/TrendingProducts";
 
-=======
+
 import HeroSection from "@/components/HeroSection";
->>>>>>> e1db73aaff5c035159e7e3371ba8872452d0e8a3
 
 export default function Home() {
   return (
