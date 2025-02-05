88a277895116d7649674ccee824d9693825e48a2
Muhammad Saad <saadsaleem997@gmail.com>
2025-02-06 01:37:21 +0500

    Resolve page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Brands from "@/app/components/Brands";
 import DiscountItem from "@/app/components/DiscountItem";
 import FeaturedProd from "@/app/components/FeaturedProd";
@@ -10,10 +9,6 @@ import Rectangle from "@/app/components/Rectangle";
 import ShopexOffer from "@/app/components/ShopexOffer";
 import TopCategory from "@/app/components/TopCategory";
 import TrendingProducts from "@/app/components/TrendingProducts";
-
-=======
-import HeroSection from "@/components/HeroSection";
->>>>>>> e1db73aaff5c035159e7e3371ba8872452d0e8a3
 
 export default function Home() {
   return (
